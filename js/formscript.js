0cfdd6f14395b0ad15183325f7db54df79afaa2a
abbie canning <30046408@live.nwrc.ac.uk>
2026-01-20 15:19:20 +0000

    Add files via upload

---
+++ js/formscript.js
@@ -0,0 +1,9 @@
+  function myFormSubmits(event) {
+      event.preventDefault(); // Prevent the default form submission
+      thanks();
+  }
+  function thanks() {
+      // Redirect to another page after download
+      window.location.href = 'Response.html'; // Replace with your desired URL
+  }// JavaScript Document
+
